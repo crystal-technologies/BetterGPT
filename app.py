9f1defe9c2091406268f21caf7b2a62ba5945110
crystal-technologies <132219723+crystal-technologies@users.noreply.github.com>
2023-06-14 15:33:58 -0400

    Add files via upload

---
+++ app.py
@@ -0,0 +1,51 @@
+from flask import Flask, request, render_template, send_file
+import CRYSTAL
+import threading
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/reply.txt')
+def serve_reply():
+    return send_file('reply.txt')
+
+@app.route('/write-file', methods=['POST'])
+def write_file():
+    data = request.get_json()
+    filename = data.get('filename')
+    file_content = data.get('data')
+
+    try:
+        with open(filename, 'w') as file:
+            file.write(file_content)
+        return 'File written successfully', 200
+    except Exception as e:
+        return str(e), 500
+
+def flask_app_runner():
+    app.run()
+
+def answer():
+    chat_history = []
+    while True:
+        with open('query.txt', 'r') as query_file:
+            query_data = query_file.read()
+        if query_data != "":
+            response, chat_history = CRYSTAL.ask_crystal(query_data, chat_history, "")
+            with open("query.txt", "w") as clear_file:
+                clear_file.write("")
+        else:
+            pass
+
+
+
+if __name__ == '__main__':
+    flask_thread = threading.Thread(target=flask_app_runner)
+    flask_thread.start()
+
+    main_thread = threading.Thread(target=answer)
+    main_thread.start()
