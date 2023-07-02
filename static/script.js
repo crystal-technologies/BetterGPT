fc1b343069f6214e9dabfac7491d877f53a5b65d
crystal-technologies <132219723+crystal-technologies@users.noreply.github.com>
2023-07-02 10:56:55 -0400

    Add files via upload

--- static/script.js
+++ static/script.js
@@ -1,58 +1,125 @@
+console.log("JS WORKING")
+
+if (window.innerWidth < "1400") {
+    document.querySelector('.sidebar').classList.add('sidebargo')
+}
+
+var queryContainer = document.querySelector('.query-container');
+
+document.querySelector('#main-content').style.height = window.innerHeight + "px";
+document.querySelector('body').style.height = window.innerHeight + "px";
+document.querySelector('.hamburger').addEventListener("click", () => {
+    document.querySelector('.sidebar').classList.toggle('sidebargo');
+    document.querySelector('.hamburger').classList.toggle = 'cross';
+    document.querySelector('.query-container').classList.toggle('querygo');
+    if (queryContainer.classList.contains('querygo')) {
+        setTimeout(function () {
+            queryContainer.style.display = 'none';
+        }, 200);
+    } else {
+        queryContainer.style.display = "flex";
+    }
+})
+
+var sendButton = document.querySelector('.send-button');
+var queryInput = document.querySelector('.query-input');
+
+sendButton.disabled = true; // Disable the send button initially
+
+
+var temperatureSlider = document.getElementById('temperature-slider');
+var temperatureValue = document.getElementById('temperature-value');
+
+var topPSlider = document.getElementById('top-p-slider');
+var topPValue = document.getElementById('top-p-value');
+
+var topKSlider = document.getElementById('top-k-slider');
+var topKValue = document.getElementById('top-k-value');
+
+var repeatPenaltySlider = document.getElementById('repeat-penalty-slider');
+var repeatPenaltyValue = document.getElementById('repeat-penalty-value');
+
+var maxTokensSlider = document.getElementById('max-tokens-slider');
+var maxTokensValue = document.getElementById('max-tokens-value');
+
+var maxHistorySlider = document.getElementById('max-history-slider');
+var maxHistoryValue = document.getElementById('max-history-value');
+
+
+temperatureSlider.addEventListener('input', function () {
+    temperatureValue.textContent = temperatureSlider.value;
+    writeParameters();
+});
+
+topPSlider.addEventListener('input', function () {
+    topPValue.textContent = topPSlider.value;
+    writeParameters();
+});
+
+topKSlider.addEventListener('input', function () {
+    topKValue.textContent = topKSlider.value;
+    writeParameters();
+});
+
+repeatPenaltySlider.addEventListener('input', function () {
+    repeatPenaltyValue.textContent = repeatPenaltySlider.value;
+    writeParameters();
+});
+
+maxTokensSlider.addEventListener('input', function () {
+    maxTokensValue.textContent = maxTokensSlider.value;
+    writeParameters();
+});
+
+maxHistorySlider.addEventListener('input', function () {
+    maxHistoryValue.textContent = maxHistorySlider.value;
+    writeParameters();
+});
+
+function writeParameters() {
+    var parameters = temperatureSlider.value + "\n" + topPSlider.value + "\n" + topKSlider.value + "\n" + repeatPenaltySlider.value + "\n" + maxTokensSlider.value + "\n" + maxHistorySlider.value;
+
+    fetch('/write-file', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ filename: 'parameters.txt', data: parameters }),
+    })
+        .then((response) => {
+            if (response.ok) {
+                console.log('Parameters written successfully.');
+            } else {
+                console.error('Error writing parameters:', response.statusText);
+            }
+        })
+        .catch((error) => {
+            console.error('Error writing parameters:', error);
+        });
+}
+
 window.addEventListener('load', function () {
     var loadingScreen = document.getElementById('loading-screen');
     var mainContent = document.getElementById('main-content');
 
-    var temperatureSlider = document.getElementById('temperature-slider');
-    var temperatureValue = document.getElementById('temperature-value');
-
-    var topPSlider = document.getElementById('top-p-slider');
-    var topPValue = document.getElementById('top-p-value');
-
-    var topKSlider = document.getElementById('top-k-slider');
-    var topKValue = document.getElementById('top-k-value');
-
-    var repeatPenaltySlider = document.getElementById('repeat-penalty-slider');
-    var repeatPenaltyValue = document.getElementById('repeat-penalty-value');
-
-    var maxTokensSlider = document.getElementById('max-tokens-slider');
-    var maxTokensValue = document.getElementById('max-tokens-value');
-
-    var maxHistorySlider = document.getElementById('max-history-slider');
-    var maxHistoryValue = document.getElementById('max-history-value');
-
-
-    temperatureSlider.addEventListener('input', function () {
-        temperatureValue.textContent = temperatureSlider.value;
-    });
-
-    topPSlider.addEventListener('input', function () {
-        topPValue.textContent = topPSlider.value;
-    });
-
-    topKSlider.addEventListener('input', function () {
-        topKValue.textContent = topKSlider.value;
-    });
-
-    repeatPenaltySlider.addEventListener('input', function () {
-        repeatPenaltyValue.textContent = repeatPenaltySlider.value;
-    });
-
-    maxTokensSlider.addEventListener('input', function () {
-        maxTokensValue.textContent = maxTokensSlider.value;
-    });
-
-    maxHistorySlider.addEventListener('input', function () {
-        maxHistoryValue.textContent = maxHistorySlider.value;
-    });
-
     var centeredContent = document.getElementById('centered-content');
-    var queryContainer = document.getElementById('query-container');
-    var queryInput = document.getElementById('query-input');
-    var sendButton = document.getElementById('send-button');
     var conversation = document.getElementById('conversation');
     var main = document.getElementById('main');
 
+    function scrollConversationToBottom() {
+        conversation.scrollTop = conversation.scrollHeight;
+    }
+
+    sendButton.disabled = true; // Disable the send button initially
+
     sendButton.addEventListener('click', function () {
+        sendButton.disabled = true; // Disable the send button initially
+        document.querySelector('.status-image').src = "https://i.pinimg.com/originals/3e/f0/e6/3ef0e69f3c889c1307330c36a501eb12.gif";
+        // sendButton.style.padding = "0";
+        document.querySelector('.send-button').style.opacity = "1";
+        // document.querySelector('.status-image').style.height = "30px";
+
+
         var query = queryInput.value.trim();
         if (query !== '') {
             main.style.display = 'none';
@@ -112,8 +179,9 @@ window.addEventListener('load', function () {
                     newReplyTextElement.classList.add("message")
                     newReplyTextElement.style.padding = "20px";
                     newReplyTextElement.textContent = text;
-                    // Append the new reply text element to the desired container in the HTML
                     conversation.appendChild(newReplyTextElement);
+                    newReplyTextElement.classList.add("cursor-animation");
+                    scrollConversationToBottom();
                 }
             }
 
@@ -121,7 +189,20 @@ window.addEventListener('load', function () {
                 const replyTextElement = document.getElementById('current-reply');
                 if (replyTextElement) {
                     replyTextElement.removeAttribute('id')
+                    replyTextElement.classList.remove("cursor-animation");
                 }
+                sendButton.disabled = true;
+                document.querySelector('.status-image').src = "./static/Send Icon.png";
+                document.querySelector('.send-button').style.opacity = "0.3";
+                
+                if (queryInput.value.trim() !== '') {
+                    document.querySelector('.send-button').style.opacity = "1";
+                    sendButton.disabled = false; // Enable the send button if there is text
+                } else {
+                    document.querySelector('.send-button').style.opacity = "0.3";
+                    sendButton.disabled = true; // Disable the send button if there is no text
+                }
+                scrollConversationToBottom();
             }
 
             function checkReplyFile() {
@@ -145,9 +226,16 @@ window.addEventListener('load', function () {
     });
 
     queryInput.addEventListener('input', function () {
+        if (queryInput.value.trim() !== '') {
+            document.querySelector('.send-button').style.opacity = "1";
+            sendButton.disabled = false; // Enable the send button if there is text
+        } else {
+            document.querySelector('.send-button').style.opacity = "0.3";
+            sendButton.disabled = true; // Disable the send button if there is no text
+        }
 
+        this.style.height = "auto";
         if (this.scrollHeight > 30) {
-            this.style.height = "20px";
             this.style.height = (this.scrollHeight > 30 ? this.scrollHeight - 10 : this.scrollHeight) + 'px';
         }
     });
