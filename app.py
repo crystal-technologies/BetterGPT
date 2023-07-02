30e4682457f22cc1ba3d513c9b689dadd58c6c20
crystal-technologies <132219723+crystal-technologies@users.noreply.github.com>
2023-07-02 10:58:41 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,33 @@
 from flask import Flask, request, render_template, send_file
-import CRYSTAL
+import datetime
 import threading
+import CRYSTAL
 
 app = Flask(__name__)
 
+def get_time():
+    currentTime = datetime.datetime.now()
+    formatted_time = "{}:{}:{} {}"
+
+    if currentTime.hour < 13:
+        hour = currentTime.hour
+        meredian = "AM"
+    else:
+        hour = currentTime.hour - 12
+        meredian = "PM"
+
+    formatted_time = f"{hour}:{currentTime.minute}:{currentTime.second} {meredian}"
+    return formatted_time
+
+@app.after_request
+def add_ngrok_header(response):
+    response.headers['ngrok-skip-browser-warning'] = 'true'
+    return response
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
 
 @app.route('/reply.txt')
 def serve_reply():
@@ -27,15 +47,17 @@ def write_file():
         return str(e), 500
 
 def flask_app_runner():
-    app.run()
+    app.run(port=7777)
 
 def answer():
     chat_history = []
     while True:
         with open('query.txt', 'r') as query_file:
             query_data = query_file.read()
+        with open('parameters.txt', 'r') as param_file:
+            params = [float(parameter) if "." in parameter else int(parameter) for parameter in param_file.readlines()]
         if query_data != "":
-            response, chat_history = CRYSTAL.ask_crystal(query_data, chat_history, "")
+            response, chat_history = CRYSTAL.ask_crystal(query_data, chat_history, f"Time: {get_time()}", username="User", finetune_param=params)
             with open("query.txt", "w") as clear_file:
                 clear_file.write("")
         else:
